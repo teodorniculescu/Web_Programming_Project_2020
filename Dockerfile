8afdd85089d9b192bac8c93a93613bb18c5b9949
teodorniculescu <teodorvicentiuniculescu@gmail.com>
2020-04-22 22:12:49 +0300

    Added nodemon, nodemon does not work, also having errors with mariadb pool

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 EXPOSE 8080
-ENTRYPOINT [ "npm", "run", "start" ]
+CMD [ "npm", "run", "start" ]
--- start.js
+++ start.js
@@ -1,11 +1,48 @@
+const dotenv = require("dotenv");
+const result = dotenv.config();
+if (result.error) {
+  throw result.error;
+}
+
 const express = require("express");
+const mariadb = require("mariadb");
 
 const app = express();
+/*
+const pool = mariadb.createPool({
+  user: process.env.MARIADB_USER,
+  password: process.env.MARIADB_PASSWORD,
+  port: process.env.MARIADB_PORT,
+  socketPath: "/var/run/mysqld/mysqld.sock",
+  connectionLimit: 5,
+});
+*/
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+console.log(process.env.MARIADB_USER);
+
+const pool = mariadb.createPool({
+  host: "127.0.0.1",
+  user: "root",
+  password: "itisoveranakin",
+  port: 3306,
+  connectionLimit: 5,
 });
 
+async function asyncFunction() {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query("SELECT 1 as val");
+    console.log(rows); //[ {val: 1}, meta: ... ]
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) return conn.end();
+  }
+}
+
 console.log("de ce plang chitarele");
+asyncFunction();
+console.log("woah");
 
 app.listen(3000);
