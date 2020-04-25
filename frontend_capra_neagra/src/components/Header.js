8c3c7bfc39967910903f92700d83ac9cea308baf
teodorniculescu <teodorvicentiuniculescu@gmail.com>
2020-04-25 03:42:43 +0300

    Changed svg and added menu class

--- frontend_capra_neagra/src/components/Header.js
+++ frontend_capra_neagra/src/components/Header.js
@@ -1,13 +1,53 @@
 import React from "react";
-import logo from "../images/capra_neagra_black.svg";
+import logo from "../images/capra_neagra.svg";
+import ham from "../images/cn_hamburger.svg";
 import styles from "./Header.module.scss";
 
-function Header() {
-  return (
-    <header>
-      <img className={styles.Logo} src={logo} alt="Capra Neagra Logo" />
-    </header>
-  );
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <nav>
+          <Logo />
+          <a href="#">
+            <img className={styles.Menu} src={ham} alt="Toggle menu" />
+          </a>
+          <Menu />
+        </nav>
+      </header>
+    );
+  }
+}
+
+class Logo extends React.Component {
+  render() {
+    return (
+      <img
+        href="/"
+        className={styles.Logo}
+        src={logo}
+        alt="Capra Neagra Logo"
+      />
+    );
+  }
+}
+
+class Menu extends React.Component {
+  render() {
+    return (
+      <ul>
+        <MenuLink name="" />
+        <MenuLink name="FAQ" />
+        <MenuLink name="." />
+      </ul>
+    );
+  }
+}
+
+class MenuLink extends React.Component {
+  render() {
+    return <li>{this.props.name}</li>;
+  }
 }
 
 export default Header;
