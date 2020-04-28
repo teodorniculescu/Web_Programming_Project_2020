c8c2cc1eed9f5453a404da30b11dda01ddea723b
teodorniculescu <teodorvicentiuniculescu@gmail.com>
2020-04-29 01:26:17 +0300

    Added register function

--- backend_capra_neagra/Users/services.js
+++ backend_capra_neagra/Users/services.js
@@ -8,24 +8,23 @@ const register = async (username, password) => {
   const role = username === "admin" ? "admin" : "user";
   const values = getValues({ username, hashedPassword, role });
   const cmd = `INSERT INTO Users (username, password, role) VALUES (${values});`;
-  query(cmd).then((res) => {
-    console.log(res);
-  });
+  await query(cmd);
 };
 
 const login = async (username, password) => {
-  const user = await Users.findOne({ username });
-  if (user === null) {
+  const values = getValues({ username });
+  const cmd = `SELECT * FROM Users WHERE username=${values};`;
+  const query_res = (await query(cmd))[0];
+  if (query_res === undefined) {
     throw new ServerError(
-      `Utilizatorul inregistrat cu ${username} nu exista!`,
+      `Utilizatorul inregistrat cu username-ul: ${username} nu exista!`,
       404
     );
   }
-
-  if (await compare(password, user.password)) {
+  if (await compare(password, query_res.password)) {
     return await generateToken({
-      userId: user._id,
-      userRole: user.role,
+      userId: query_res.id,
+      userRole: query_res.role,
     });
   }
   throw new ServerError("Combinatia de username si parola nu este buna!", 404);
