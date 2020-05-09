48191f39968616304af71bdaec165ddafa083e3a
teodorniculescu <teodorvicentiuniculescu@gmail.com>
2020-05-09 13:40:29 +0300

    Finished footer

--- frontend_capra_neagra/src/components/Footer/Footer.js
+++ frontend_capra_neagra/src/components/Footer/Footer.js
@@ -1,72 +1,34 @@
 import React from "react";
 import styles from "./Footer.module.scss";
+import { Link } from "react-router-dom";
 
 class Footer extends React.Component {
-  getLeftContainer() {
-    /*
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-6">
-            <h6>About</h6>
-            <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
-          </div>
-
-          <div class="col-xs-6 col-md-3">
-            <h6>Categories</h6>
-            <ul class="footer-links">
-              <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
-              <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
-              <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
-              <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
-              <li><a href="http://scanfcode.com/category/android/">Android</a></li>
-              <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
-            </ul>
-          </div>
-
-          <div class="col-xs-6 col-md-3">
-            <h6>Quick Links</h6>
-            <ul class="footer-links">
-              <li><a href="http://scanfcode.com/about/">About Us</a></li>
-              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
-              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
-              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
-              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
-            </ul>
-          </div>
-        </div>
-        <hr>
-      </div>
-    */
-    return null;
-  }
-  getRightContainer() {
-    /*
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-sm-6 col-xs-12">
-            <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
-         <a href="#">Scanfcode</a>.
-            </p>
-          </div>
-
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <ul class="social-icons">
-              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
-              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
-            </ul>
-          </div>
-        </div>
-      </div>
-    */
-    return null;
+  getLink(to, text) {
+    return (
+      <li>
+        <Link to={to}>{text}</Link>
+      </li>
+    );
   }
   render() {
     return (
       <footer className={styles.site_footer}>
-        {this.getLeftContainer()}
-        {this.getRightContainer()}
+        <div className={styles.container}>
+          <hr></hr>
+          <div className={(styles.col_xs_6, styles.col_md_3)}>
+            <h6>Informatii</h6>
+            <ul className={styles.footer_links}>
+              {this.getLink("/ana3", "Suport tehnic")}
+              {this.getLink("/ana2", "Contactează-ne")}
+              {this.getLink("/ana4", "Termeni și condiții")}
+              {this.getLink("/ana1", "Întrebări frecvente")}
+            </ul>
+          </div>
+          <hr></hr>
+          <div className={styles.copyright_text}>
+            © Capra Neagra 2020 All Rights Reserved
+          </div>
+        </div>
       </footer>
     );
   }
