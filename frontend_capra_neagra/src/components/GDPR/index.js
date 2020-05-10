75d900373482db746616ad5f2eb2cb1f70478d29
teodorniculescu <teodorvicentiuniculescu@gmail.com>
2020-05-10 14:46:09 +0300

    Finished gdpr

--- frontend_capra_neagra/src/components/GDPR/index.js
+++ frontend_capra_neagra/src/components/GDPR/index.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
 import styles from "./Styles.module.scss";
 class GDPR extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { performance: true, publicity: true };
+    this.handlePerformance = this.handlePerformance.bind(this);
+    this.handlePublicity = this.handlePublicity.bind(this);
+  }
+
   state = {};
-  hideAll() {
+  getStringBool(value) {
+    return value ? "true" : "false";
+  }
+  setPreferences() {
+    localStorage.setItem(
+      "Performance",
+      this.getStringBool(this.state.performance)
+    );
+    localStorage.setItem("Publicity", this.getStringBool(this.state.publicity));
+  }
+  hideAll = () => {
     document.getElementById("gdpr_dark_bg").style.visibility = "hidden";
     document.getElementById("gdpr_about").style.visibility = "hidden";
     document.getElementById("gdpr_manage").style.visibility = "hidden";
-  }
+    this.enableScroll();
+    this.setPreferences();
+  };
   visibilityManage() {
     document.getElementById("gdpr_manage").style.visibility = "visible";
   }
@@ -16,13 +35,13 @@ class GDPR extends Component {
   getAboutText() {
     return (
       <div>
-        DESPRE COOKIES Noi şi partenerii noştri procesăm informaţiile despre
-        dumneavoastră, despre dispozitivele dumneavoastră şi comportamentul
-        dumneavoastră online utilizând tehnologii cum ar fi cookies pentru a
-        presta, analiza şi îmbunătăţi serviciile noastre; pentru a personaliza
-        conţinutul sau pentru a face publicitate pe acest site sau pe altele, pe
-        aplicaţii sau platforme şi pentru a furniza caracteristici de rețele
-        sociale.
+        <h3>DESPRE COOKIES</h3>
+        Noi şi partenerii noştri procesăm informaţiile despre dumneavoastră,
+        despre dispozitivele dumneavoastră şi comportamentul dumneavoastră
+        online utilizând tehnologii cum ar fi cookies pentru a presta, analiza
+        şi îmbunătăţi serviciile noastre; pentru a personaliza conţinutul sau
+        pentru a face publicitate pe acest site sau pe altele, pe aplicaţii sau
+        platforme şi pentru a furniza caracteristici de rețele sociale.
         <br></br>
         <br></br>
         Doriţi să acceptaţi cookie-uri?
@@ -43,7 +62,7 @@ class GDPR extends Component {
           <button
             className={styles.manage_button}
             type="button"
-            onClick={this.displayManage}
+            onClick={this.visibilityManage}
           >
             Gestionare module cookie
           </button>
@@ -51,19 +70,113 @@ class GDPR extends Component {
       </div>
     );
   }
-  getManageText() {
+  getSlider(title, description, setFunction) {
     return (
-      <div>
-        GESTIONAŢI-VĂ SETĂRILE Vă rugăm alegeţi mai jos setările pentru acest
-        site. Puteţi activa/permite sau dezactiva/respinge cookie-uri pe
-        categorii sau individual. Setările dumneavoastră nu se vor aplica
-        automat pe toate companiile aparţinând Familiei de firme Walt Disney pe
-        care le vizitaţi. Vă puteţi modifica setările oricând revenind pe acest
-        site şi accesând link-ul Setări Cookies. Pentru informaţii suplimentare
-        vă rugăm citiți Politica noastră sau politica terţelor părţi pe
-        linkurile disponibile mai jos.
+      <div className={styles.slider_container}>
+        <div className={styles.slider_area}>
+          <div className={styles.left}>{title}</div>
+          <div className={styles.right}>
+            <label className={styles.switch}>
+              <input
+                type="checkbox"
+                defaultChecked={true}
+                onClick={setFunction}
+              ></input>
+              <span className={styles.slider}></span>
+            </label>
+          </div>
+        </div>
+        <br></br>
+        <div>{description}</div>
+        <br></br>
       </div>
     );
+  }
+  getMangatory() {
+    const title = "Necesare";
+    const description =
+      "Esenţial pentru a furniza acest serviciu sau alte caracteristici. ";
+    return (
+      <div className={styles.slider_container}>
+        <div className={styles.slider_area}>
+          <div className={styles.left}>{title}</div>
+          <div className={styles.right}>Active Permanent ✓</div>
+        </div>
+        <br></br>
+        <div>{description}</div>
+        <br></br>
+      </div>
+    );
+  }
+  getManageButton() {
+    return (
+      <div className={styles.about_buttons}>
+        <button
+          className={styles.yes_button}
+          type="button"
+          onClick={this.hideAll}
+        >
+          Salveaza setarile mele
+        </button>
+        <button
+          className={styles.anulare_button}
+          type="button"
+          onClick={this.hideManage}
+        >
+          Anulare
+        </button>
+      </div>
+    );
+  }
+  getManageText() {
+    const text1 = "Performanţă & Analiză";
+    const text2 = "Publicitate";
+    const descr1 =
+      "Utilizat de noi sau de parteneri terţe părţi pentru a măsura şi a analiza performanţa şi utilizarea siteului şi pentru a ajuta la îmbunătăţirea serviciilor noastre. ";
+    const descr2 =
+      "Utilizată de către noi sau de o terţă parte pentru a colecta informaţii privind comportamentul dumneavoastră online sau privind locaţia pentru a prezenta conţinutul relevant şi personalizat sau publicitate pe acesta sau pe alte pagini, aplicaţii sau platforme, inclusiv rețele sociale. ";
+    return (
+      <div>
+        <div className={styles.manage_title}>
+          <h3>GESTIONAŢI-VĂ SETĂRILE</h3>
+        </div>
+        <hr></hr>
+        Vă rugăm alegeţi mai jos setările pentru acest site. Puteţi
+        activa/permite sau dezactiva/respinge cookie-uri pe categorii sau
+        individual. Setările dumneavoastră nu se vor aplica automat pe toate
+        companiile aparţinând Familiei de firme Walt Disney pe care le vizitaţi.
+        Vă puteţi modifica setările oricând revenind pe acest site şi accesând
+        link-ul Setări Cookies.
+        <br></br>
+        <br></br>
+        Pentru informaţii suplimentare vă rugăm citiți Politica noastră sau
+        politica terţelor părţi pe linkurile disponibile mai jos.
+        <br></br>
+        <br></br>
+        <br></br>
+        {this.getMangatory()}
+        {this.getSlider(text1, descr1, this.handlePerformance)}
+        {this.getSlider(text2, descr2, this.handlePublicity)}
+        {this.getManageButton()}
+      </div>
+    );
+  }
+
+  handlePerformance() {
+    this.setState({ performance: !this.state.performance });
+  }
+  handlePublicity() {
+    this.setState({ publicity: !this.state.publicity });
+  }
+
+  disableScroll() {
+    document.body.style.overflowY = "hidden";
+  }
+  enableScroll() {
+    document.body.style.overflowY = "visible";
+  }
+  componentDidMount() {
+    this.disableScroll();
   }
   render() {
     return (
